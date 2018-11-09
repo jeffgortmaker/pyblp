1683a9a1b0b228cc42e22e5cfacce60517970f38
Jeff Gortmaker <jeff@jeffgortmaker.com>
2018-11-09 08:32:51 -0500

    MNT: revert to faster GMM moment Jacobian computation method

--- pyblp/utilities/statistics.py
+++ pyblp/utilities/statistics.py
@@ -146,6 +146,13 @@ def compute_gmm_moments_mean(u_list: List[Array], Z_list: List[Array]) -> Array:
 
 def compute_gmm_moments_jacobian_mean(jacobian_list: List[Array], Z_list: List[Array]) -> Array:
     """Compute the Jacobian of GMM moment conditions with respect to parameters, averaged across observations."""
+
+    # tensors or loops are not needed when there is only one equation
+    if len(jacobian_list) == 1:
+        N = Z_list[0].shape[0]
+        return Z_list[0].T @ jacobian_list[0] / N
+
+    # tensors are faster than loops for more than one equation
     Z_transpose_stack = np.dstack(np.split(scipy.linalg.block_diag(*Z_list), len(jacobian_list)))
     jacobian_stack = np.dstack(jacobian_list).swapaxes(1, 2)
     return (Z_transpose_stack @ jacobian_stack).mean(axis=0)
