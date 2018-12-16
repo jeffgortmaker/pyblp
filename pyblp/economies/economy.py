BUG: handle data type issues from explicit broadcasting

pyblp/economies/economy.py
@@ -7,6 +7,7 @@ from typing import Dict, Hashable, Mapping, Optional, Sequence, Tuple
 
 import numpy as np
 
+from .. import options
 from ..configurations.formulation import ColumnFormulation, Formulation
 from ..utilities.basics import Array, RecArray, StringRepresentation, TableFormatter
 
@@ -175,7 +176,8 @@ class Economy(abc.ABC, StringRepresentation):
         columns = []
         for include, formulation in zip(index, self._X1_formulations):
             if include:
-                columns.append(np.broadcast_to(formulation.evaluate(self.products, data_override), (self.N, 1)))
+                column = formulation.evaluate(self.products, data_override)
+                columns.append(np.broadcast_to(column, (self.N, 1)).astype(options.dtype))
         return np.column_stack(columns)
 
     def _compute_true_X3(

pyblp/parameters.py
@@ -104,7 +104,7 @@ class LinearCoefficient(Coefficient):
     def get_product_characteristic(self, economy_or_market: Union['Economy', 'Market']) -> Array:
         """Get the product characteristic associated with the parameter."""
         x = self.get_product_formulation(economy_or_market).evaluate(economy_or_market.products)
-        return np.broadcast_to(x, (economy_or_market.products.shape[0], 1))
+        return np.broadcast_to(x, (economy_or_market.products.shape[0], 1)).astype(options.dtype)
 
 
 class BetaParameter(LinearCoefficient):