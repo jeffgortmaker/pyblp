e0386929536114170c1d543a31931b7335081b3a
Jeff Gortmaker <jeff@jeffgortmaker.com>
2020-01-11 15:07:26 -0500

    DOC: update the BLP example data to more closely match the original paper

--- pyblp/data/__init__.py
+++ pyblp/data/__init__.py
@@ -7,7 +7,7 @@ NEVO_PRODUCTS_LOCATION : `str`
     the same pre-computed excluded instruments used in the original paper.
 NEVO_AGENTS_LOCATION : `str`
     Location of a CSV file containing the fake cereal agent data. Included in the file are Monte Carlo weights and
-    draws along with demographics, which are used by :ref:`references:Nevo (2000)` to solve the fake cereal problem.
+    draws along with demographics from the original paper.
 BLP_PRODUCTS_LOCATION : `str`
     Location of a CSV file containing the automobile product data extracted by
     :ref:`references:Andrews, Gentzkow, and Shapiro (2017)` from the original GAUSS code for
@@ -15,45 +15,13 @@ BLP_PRODUCTS_LOCATION : `str`
     :ref:`references:Berry, Levinsohn, and Pakes (1995)`.
 
     The file also includes a set of excluded instruments. First, "sums of characteristics" BLP instruments from the
-    original paper were computed with :func:`~pyblp.build_blp_instruments`. As in the original paper, the ``mpd``
-    variable was added to the set of excluded instruments for supply. Due to a collinearity problem with these original
-    instruments, each set of excluded instruments was then interacted up to the second degree, standardized, replaced
-    with the minimum set of principal components that explained at least 99% of the variance, and standardized again.
+    original paper were computed with :func:`~pyblp.build_blp_instruments`. As in the original paper, the "rival"
+    instrument constructed from the ``trend`` variable was excluded due to collinearity issues, and the ````mpd``
+    variable was added to the set of excluded instruments for supply.
 
 BLP_AGENTS_LOCATION : `str`
-    Location of a CSV file containing automobile agent data. Included in the file are 200 Monte Carlo weights and draws
-    for each market, which, unlike in the fake cereal data, are not the same draws used in the original paper.
-
-    Also included is an income demographic, which consists of draws from lognormal distributions. The log of income has
-    a common standard deviation, ``1.72``, and the following market-varying means:
-
-    ====  =======
-    Year  Mean
-    ====  =======
-    1971  2.01156
-    1972  2.06526
-    1973  2.07843
-    1974  2.05775
-    1975  2.02915
-    1976  2.05346
-    1977  2.06745
-    1978  2.09805
-    1979  2.10404
-    1980  2.07208
-    1981  2.06019
-    1982  2.06561
-    1983  2.07672
-    1984  2.10437
-    1985  2.12608
-    1986  2.16426
-    1987  2.18071
-    1988  2.18856
-    1989  2.21250
-    1990  2.18377
-    ====  =======
-
-    These numbers were extracted also extracted by :ref:`references:Andrews, Gentzkow, and Shapiro (2017)` from the
-    original GAUSS code for :ref:`references:Berry, Levinsohn, and Pakes (1999)`.
+    Location of a CSV file containing the automobile agent data. Included in the file are the importance sampling
+    weights and draws along with the income demographic from the original paper.
 
 Examples
 --------
