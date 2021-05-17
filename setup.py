ebdfd7b17b1d6ebed2ca7421c2d7494c79862fe6
Jeff Gortmaker <jeff@jeffgortmaker.com>
2021-05-17 14:28:56 -0400

    BLD/DOC: pin jinja2 version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'tests': ['pytest', 'pytest-xdist', 'linearmodels'],
         'docs': [
             'sphinx==2.0.0', 'pandas', 'ipython', 'matplotlib', 'astunparse', 'sphinx-rtd-theme==0.4.3',
-            'nbsphinx==0.5.0'
+            'nbsphinx==0.5.0', 'jinja2~=2.11',
         ],
     },
     include_package_data=True,
